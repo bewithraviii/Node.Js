52fb73a3cf18be34a509dcc7b12b89ea45d94f34
Ravi J Patel <PCA159@server7.com>
2023-06-26 10:36:44 +0530

    Completed Shop Application in MongoDB

---
+++ SHOP_Mongoose/Old/firstnode.js
@@ -0,0 +1,20 @@
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('Hello World!');
+  res.write(req.url);
+  console.log(req.url);
+  res.end();
+}).listen(8080);
+
+
+// var url = require('url');
+// var adr = 'http://localhost:8080/default.htm?year=2017&month=february';
+// var q = url.parse(adr, true);
+
+// console.log(q.host); //returns 'localhost:8080'
+// console.log(q.pathname); //returns '/default.htm'
+// console.log(q.search); //returns '?year=2017&month=february'
+
+// var qdata = q.query; //returns an object: { year: 2017, month: 'february' }
+// console.log(qdata.month); //returns 'february'
